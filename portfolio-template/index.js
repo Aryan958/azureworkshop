169ad3ff6d39947902576af7cd64be3287fb1d4f
Aryan Raj <98835041+Aryan958@users.noreply.github.com>
2022-05-05 16:05:04 +0530

    Update index.js
    updated my name and email

--- portfolio-template/index.js
+++ portfolio-template/index.js
@@ -1,6 +1,6 @@
-const name = 'Your Name'
-const title = 'Your Title'
-const mail = 'Your Email'
+const name = 'Aryan raj'
+const title = 'WEB DEVLOPER'
+const mail = 'aryanraj122333@gmail.com'
 
 document.getElementById('title').innerText = name
 document.getElementById('name').innerText = name
